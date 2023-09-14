abb8b978af5d2784fea5c790b486061e2fe1ab1b
pdriding <peterdriding@gmail.com>
2023-09-14 13:12:16 +0400

    Initial commit

--- app.js
+++ app.js
@@ -1,32 +1,131 @@
+// Query Selectors
 
-    // Create Dino Constructor
+const humanBtn = document.querySelector("#btn");
 
+humanBtn.addEventListener("click", function (e) {
+  console.log(e);
+});
 
-    // Create Dino Objects
+// Create Dino Constructor
 
+class Dino {
+  constructor(species, weight, height, diet, where, when, fact) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+  }
+}
 
-    // Create Human Object
+// Create Dino Objects
 
-    // Use IIFE to get human data from form
+const triceratops = new Dino(
+  "triceratops",
+  13000,
+  114,
+  "herbavor",
+  "North America",
+  "Late Cretaceous",
+  "First discovered in 1889 by Othniel Charles Marsh"
+);
 
+const tRex = new Dino(
+  "Tyrannosaurus Rex",
+  11905,
+  144,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "The largest known skull measures in at 5 feet long."
+);
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+const anklyosaurus = new Dino(
+  "Anklyosaurus",
+  10500,
+  55,
+  "herbavor",
+  "North America",
+  "Late Cretaceous",
+  "Anklyosaurus survived for approximately 135 million years."
+);
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+const brachiosaurus = new Dino(
+  "Brachiosaurus",
+  70000,
+  "372",
+  "herbavor",
+  "North America",
+  "Late Jurasic",
+  "An asteroid was named 9954 Brachiosaurus in 1991."
+);
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+const stegosaurus = new Dino(
+  "Stegosaurus",
+  11600,
+  79,
+  "herbavor",
+  "North America, Europe, Asia",
+  "Late Jurasic to Early Cretaceous",
+  "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+);
 
+const elasmosaurus = new Dino(
+  "Elasmosaurus",
+  16000,
+  59,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "Elasmosaurus was a marine reptile first discovered in Kansas."
+);
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+const pteranodon = new Dino(
+  "Pteranodon",
+  44,
+  20,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "Actually a flying reptile, the Pteranodon is not a dinosaur."
+);
 
-    // Remove form from screen
+const pigeon = new Dino(
+  "Pigeon",
+  0.5,
+  9,
+  "herbavor",
+  "World Wide",
+  "Holocene",
+  "All birds are living dinosaurs."
+);
 
+// Create Human Object
+
+class Human extends Dino {
+  constructor(name, height, weight, diet) {
+    super(height, weight, diet);
+    this.name = name;
+  }
+}
+
+// Use IIFE to get human data from form
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 // On button click, prepare and display infographic
